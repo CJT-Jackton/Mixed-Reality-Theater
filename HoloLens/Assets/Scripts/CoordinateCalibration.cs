aaf8a0b9954a760b028b471c7de6c38e94009872
CJT <jietong_chen@outlook.com>
2019-04-17 21:11:57 -0400

    Add Vuforia image tracking

---
+++ HoloLens/Assets/Scripts/CoordinateCalibration.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.XR.WSA;
+using Vuforia;
+
+public class CoordinateCalibration : DefaultTrackableEventHandler
+{
+    public Transform WorldOrigin;
+    public Transform ImageTarget;
+
+    #region PROTECTED_METHODS
+
+    protected override void OnTrackingFound()
+    {
+        Debug.Log("Image found!");
+
+        WorldOrigin.position = ImageTarget.position;
+        WorldOrigin.rotation = ImageTarget.rotation;
+
+        WorldOrigin.gameObject.AddComponent<WorldAnchor>();
+
+        TrackerManager.Instance.GetTracker<ObjectTracker>().Stop();
+    }
+
+    #endregion
+}
